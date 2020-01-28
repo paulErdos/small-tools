d33d78699e7520c03749729b0d35e1f67fa0cca7
Vincent Steffens <vincesteffens@gmail.com>
2020-01-28 11:34:38 -0800

    Add notes

--- user-config.sh
+++ user-config.sh
@@ -6,4 +6,7 @@ git config user.email "vincesteffens@gmail.com"
 # Verify by cat .git/config
 
 # Q: Does this need to be sourced?
-# A: no.
+# A: no, but it still needs to be run.
+
+# Q: What if I set the username and email of above to something else?
+# A: Commits aren't credited in contribution activity.
