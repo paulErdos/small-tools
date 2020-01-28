46f65716d51d8a2f18d7663fafcd266c62f570e3
Vincent Steffens <vincesteffens@gmail.com>
2020-01-28 11:29:15 -0800

    Attempt to automate user config st commits are credited

---
+++ source-this.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Set repository-specific uname/email config:
+git config user.name "Vincent Steffens"
+git config user.email "vincesteffens@gmail.com"
+# Verify by cat .git/config
+
+# Q: Does this need to be sourced?
