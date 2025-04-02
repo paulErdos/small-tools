7e50c364854648e4ddaf04c01df8b6c513ef82e3
neon <vincesteffens@gmail.com>
2025-04-01 22:25:10 -0700

    Fix setup

---
+++ in-development/pre-commit.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+echo 'literally nothing yet'
--- setup.sh
+++ setup.sh
@@ -1,27 +1,3 @@
 #!/bin/bash
 
-#if [ "$#" != 1 ]; then
-#    echo '================================================='
-#    echo "usage: $0 name-of-tool-to-add-to-path"
-#    echo '================================================='
-#    echo
-#    exit 1
-#fi
-#
-#if [ ! -d in-path ]; then
-##    echo
-##    echo '============================================'
-##    echo '    you must edit your own path yourself    '
-##    echo '============================================'
-##    echo
-##    echo '   The line you want is...'
-##    echo "$pwd"
-##    echo "   $(pwd)"
-##    echo
-##    echo
-#    mkdir -p in-path  # unnecessary -p
-#fi
-#
-#cp $1 in-path
-#echo $PATH | sed s,:,n,g | grep "$(pwd)"
-#cat ~/.bashrc | grep "$(pwd)"
+echo "PATH=$PATH:$(pwd)" >> ~/.bashrc && bash
