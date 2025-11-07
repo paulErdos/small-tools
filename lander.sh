71f0f286e3ebfd690089a90ad7ebe9432d207e98
paulErdos <vincesteffens@gmail.com>
2025-11-07 01:08:00 -0800

    Stared standard landing operation

---
+++ lander.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Clone standard repos 
+git clone git@github.com:paulErdos/Practice-Prototypes.git
+git clone git@github.com:paulErdos/NutritionFacts.git
+git clone git@github.com:paulErdos/vim-bash-RCs.git
+git clone git@github.com:paulErdos/small-tools.git
+
+
